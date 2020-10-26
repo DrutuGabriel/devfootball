0e206c81a70ca570545e04a64baf99bfa1adf0b9
Gabriel D <gabriel.drutu@gmail.com>
2020-10-26 10:01:12 +0200

    Matches and meet the players component

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+const config = {
+  apiKey: "AIzaSyCB76cWA_YmSc_mSVgY22jsBiAqpy1xD_c",
+  authDomain: "m-city-5fe98.firebaseapp.com",
+  databaseURL: "https://m-city-5fe98.firebaseio.com",
+  projectId: "m-city-5fe98",
+  storageBucket: "m-city-5fe98.appspot.com",
+  messagingSenderId: "45993700517",
+  appId: "1:45993700517:web:9220d4c7ff8310ede3b14c"
+};
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref('matches');
+
+export {
+  firebase,
+  firebaseMatches
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './routes';
 
 import './Resources/css/app.css';
 
-import {BrowserRouter} from 'react-router-dom';
-import Routes from './routes';
 
 const App = () => {
   return (
