32d8197c259eac7564b78373dbe9845243b36f33
Gabriel D <gabriel.drutu@gmail.com>
2020-12-01 08:56:51 +0200

    File Uploader continued

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const config = {
   apiKey: "AIzaSyCB76cWA_YmSc_mSVgY22jsBiAqpy1xD_c",
